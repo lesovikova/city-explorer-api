0531b063c1267450ad3e0ada22ddcd3df39fff23
__lesovikova__ <__lesovikova@gmail.com__>
2023-07-28 08:24:56 +0100

    refactor api city req to server from client

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express");
-
 const cors = require("cors");
-
+const axios = require("axios");
 require("dotenv").config();
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
-
 app.use(cors());
+const res = require("express/lib/response");
 
 const weatherData = require("./data/weather.json");
-const res = require("express/lib/response");
+const city = require("./lib/city");
+
 
 function findCity(data, searchItems) {
     const searchObject = {city_name:searchItems.query.city_name, lon: +searchItems.query.lon, lat: +searchItems.query.lat};
     console.log(searchObject);
-    // const keys = Object.keys(searchItems);
     const keys = Object.keys(searchObject)
     let result;
    for(let i= 0; i < keys.length; i++){
@@ -34,36 +31,22 @@ function findCity(data, searchItems) {
 
 }
 
-// findCity(weatherData, {city_name:"Paris", lon:2.35, lat:48.86})
 
-// function findCity(item){
-//     const theGame = weatherData.find((game) => game.lon == item);
-//     return theGame;
-// }
+
+app.get("/city", city);
 
 app.get("/weather", (req, res) => {
     let data = weatherData;
     let dataToReturn = data;
-    // findCity(data, req)
     
     dataToReturn = findCity(data, req)
-   
-    // let dataToReturn = findCity(req.query.lon)
-    // res.json(data)
+
     res.json(dataToReturn)
-    // res.json(findCity("Paris", 2.35, 48.86))
 })
 
 app.get("/", (req, res) => {
     res.json("you've reached the root")
 })
-
-// if (request.query.title) {
-//     dataToReturn = fitlerGamesByTitle(request.query.title);
-//   } else if (request.query.year) {
-//     dataToReturn = filterGamesByYear(request.query.year);
-//   }
-
 
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
