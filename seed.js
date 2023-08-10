86c9c25f136d49188fdded0c1a20aa2618d6a2a0
__lesovikova__ <__lesovikova@gmail.com__>
2023-08-10 17:57:08 +0100

    add the initial function

--- seed.js
+++ seed.js
@@ -5,16 +5,16 @@ const Weather = require("./models/weatherDay");
 
 async function seed() {
     await Weather.create({        
-    date: "2023-08-08",
-    tempreture: 20,
-    windSpeed: 0.5,
-    pressure: 1000,
-    clouds: 50,
-    humidity: 70,
-    snow: 0,
-    sunrise: "04:25",
-    sunset: "19:30",
-    comments: "It was a fine day, could wear a t-shirt",
+        date: "2023-08-08",
+        tempreture: 20,
+        windSpeed: 0.5,
+        pressure: 1000,
+        clouds: 50,
+        humidity: 70,
+        snow: 0,
+        sunrise: "04:25",
+        sunset: "19:30",
+        comments: "It was a fine day, could wear a t-shirt",
     });
     await Weather.create({        
         date: "2023-08-09",
--- server.js
+++ server.js
@@ -18,13 +18,34 @@ const city = require("./lib/city");
 const currentWeather = require("./lib/current_weather");
 
 
-
-
-
-
 app.get("/city", city);
 app.get("/currentweather", currentWeather);
 
+
+async function checkDate(){
+    const todayDate = new Date();
+    const thisTodayDate = todayDate.toISOString().slice(0, 10);
+    const thisDate = await Weather.find();
+    if(!thisDate[thisDate.length-1].date.includes(thisTodayDate)){
+        const API = `https://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&lat=52.6285576&lon=1.2923954`;
+        const response = await axios.get(API);
+                const newEntry = await Weather.create({
+                    date: response.data.data[0].ob_time,
+                    tempreture: response.data.data[0].temp,
+                    windSpeed: response.data.data[0].wind_spd,
+                    pressure: response.data.data[0].pres,
+                    clouds: response.data.data[0].clouds,
+                    humidity: response.data.data[0].rh,
+                    snow: response.data.data[0].snow,
+                    sunrise: response.data.data[0].sunrise,
+                    sunset: response.data.data[0].sunset,
+                    comments: "Enter comment",
+                })
+        }
+        else console.log("The item found");
+}
+
+checkDate();
 
 
 app.get("/getPastweather", async (req, res) => {
